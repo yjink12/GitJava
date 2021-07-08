546dc0854a15ab4ac03a32bdbd3d339b760334fd
yjin <yjin.k12@gmail.com>
2021-07-08 12:39:41 +0900

    hello3 추가 hello 파일에는 요일에 관한 정보 및 안부인사

--- src/githello/Hello.java
+++ src/githello/Hello.java
@@ -5,6 +5,10 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
+		
+		System.out.println("안녕하세요 홍길동님");
+		System.out.println("만나서 반갑습니다");
+		System.out.println("오늘은 목요일입니다");
 	}
 
 }
