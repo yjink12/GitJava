a10ae24a5ec8a9292008758e94a5e31d7a813d5f
yjin <yjin.k12@gmail.com>
2021-07-08 14:22:00 +0900

    conflict test

--- src/githello/Hello.java
+++ src/githello/Hello.java
@@ -5,9 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-		
+		System.out.println("월요일부터 집에 있음");
 		System.out.println("안녕하세요 홍길동님");
-		System.out.println("만나서 반갑습니다");
+		System.out.println("hello2");
 		System.out.println("오늘은 목요일입니다");
 	}
 
